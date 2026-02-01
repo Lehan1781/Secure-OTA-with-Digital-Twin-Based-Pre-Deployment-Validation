830746a224bb8dd85b0c88b4915b5d8656b523fa
tarushi-singh <tarushi.singh@eulermotors.com>
2026-02-01 22:05:57 +0530

    memory partitioning

--- flash.bat
+++ flash.bat
@@ -1,14 +1,29 @@
 @echo off
+setlocal
 
-set ELF_FILE=build\bootloader.elf
+:: -------------------------------
+:: Select target to flash
+:: -------------------------------
+echo Select target to flash:
+echo 1. Bootloader
+echo 2. App Slot A
+echo 3. App Slot B
+set /p TARGET_CHOICE=Enter choice (1/2/3): 
 
+if "%TARGET_CHOICE%"=="1" set ELF_FILE=build\bootloader.elf
+if "%TARGET_CHOICE%"=="2" set ELF_FILE=build\app_slotA.elf
+if "%TARGET_CHOICE%"=="3" set ELF_FILE=build\app_slotB.elf
+
+:: -------------------------------
+:: Flashing
+:: -------------------------------
 echo ======================================
-echo Flashing STM32 Bootloader
+echo Flashing %ELF_FILE%
 echo ======================================
 
 if not exist %ELF_FILE% (
     echo ERROR: ELF file not found.
-    echo Please run build.bat first.
+    echo Please build the project first.
     exit /b 1
 )
 
@@ -22,6 +37,12 @@ if errorlevel 1 goto error
 echo.
 echo ======================================
 echo FLASH SUCCESSFUL
+if "%TARGET_CHOICE%"=="2" (
+    echo Partitioning done for Slot A
+)
+if "%TARGET_CHOICE%"=="3" (
+    echo Partitioning done for Slot B
+)
 echo ======================================
 goto end
 
